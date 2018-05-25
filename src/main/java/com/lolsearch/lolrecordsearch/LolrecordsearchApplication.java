50eb743169ec720a30d316f321dec23b033990bc
Kwon <pporotoss@gmail.com>
2018-05-25 16:44:05 +0900

    프로젝트 생성.

---
+++ src/main/java/com/lolsearch/lolrecordsearch/LolrecordsearchApplication.java
@@ -0,0 +1,12 @@
+package com.lolsearch.lolrecordsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolrecordsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LolrecordsearchApplication.class, args);
+    }
+}
